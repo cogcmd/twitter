c3cd9fa7d56e8e9bc0b3452d2ef6a1d3cadfae72
Christopher Maier <chris@operable.io>
2016-06-02 11:01:15 -0400

    Actually tweet

--- lib/cog_cmd/twitter/tweet.rb
+++ lib/cog_cmd/twitter/tweet.rb
@@ -1,7 +1,19 @@
 require 'cog/command'
+require 'twitter'
 
 class CogCmd::Twitter::Tweet < Cog::Command
   def run_command
-    response.content = {"tweet" => request.args[0]}
+
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+      config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+    end
+
+    tweet = client.update(request.args[0])
+    response.template = "tweet"
+    response.content = {"tweet_url" => tweet.url,
+                        "tweet_text" => tweet.full_text}
   end
 end
