a3a0ab600b038acb67a6624990718a7044792525
Christopher Maier <chris@operable.io>
2016-06-03 06:02:10 -0400

    Documentation and cleanup

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:3.3
 
-RUN apk -U add ca-certificates ruby ruby-bundler ruby-dev ruby-io-console ruby-builder ruby-irb ruby-rdoc ruby-json
+RUN apk -U add ca-certificates ruby ruby-bundler ruby-dev ruby-io-console ruby-builder ruby-irb ruby-rdoc ruby-json build-base git
 
 # Setup bundle user and directory
 RUN adduser -h /home/bundle -D bundle && \
@@ -10,13 +10,10 @@ RUN adduser -h /home/bundle -D bundle && \
 # Copy the bundle source to the image
 WORKDIR /home/bundle
 COPY . /home/bundle
-RUN chmod +x /home/bundle/cog-command
 
-# Install git and build tools, run Bundler, and uninstall git and the
-# tooling to recover space
-RUN apk add git && \
-    apk add build-base && \
-    su bundle -c 'bundle install --path .bundle' && \
+# Install the bundle, and remove git and the build tooling to recover
+# some space
+RUN su bundle -c 'bundle install --path .bundle' && \
     apk del build-base && \
     apk del git && \
     rm -f /var/cache/apk/*
--- lib/cog_cmd/twitter/tweet.rb
+++ lib/cog_cmd/twitter/tweet.rb
@@ -2,18 +2,37 @@ require 'cog/command'
 require 'twitter'
 
 class CogCmd::Twitter::Tweet < Cog::Command
+
   def run_command
+    client = Twitter::REST::Client.new(user_credentials!(request))
 
-    client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-      config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-      config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+    begin
+      tweet = client.update(request.args.join(" "))
+
+      response.template = "tweet"
+      response.content  = {"tweet_url"    => tweet.url,
+                           "tweet_text"   => tweet.full_text,
+                           "account_name" => tweet.user.screen_name}
+    rescue Twitter::Error::Unauthorized
+      fail("Could not authenticate; check your credentials.")
     end
-
-    tweet = client.update(request.args[0])
-    response.template = "tweet"
-    response.content = {"tweet_url" => tweet.url,
-                        "tweet_text" => tweet.full_text}
   end
+
+  # Generate a map of Twitter client credentials from environment
+  # variables supplied in the request, or fail if any required
+  # variable is not present.
+  def user_credentials!(request)
+    user = request.options["as"] ||
+           env_var("TWITTER_DEFAULT_ACCOUNT",
+                   failure_message: "No --as option given, and no TWITTER_DEFAULT_ACCOUNT specified")
+
+    user = user.upcase
+
+    {consumer_key: env_var("TWITTER_CONSUMER_KEY", suffix: user),
+     consumer_secret: env_var("TWITTER_CONSUMER_SECRET", suffix: user),
+     access_token: env_var("TWITTER_ACCESS_TOKEN", suffix: user),
+     access_token_secret: env_var("TWITTER_ACCESS_TOKEN_SECRET", suffix: user)}
+  end
+
+
 end
